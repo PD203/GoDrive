7a993997f62f4df4c9245154bcc225669ca31d37
Preeti Dalai <93539836+PD203@users.noreply.github.com>
2025-01-10 20:55:11 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(cors())
 
 app.use(
   cors({
-    origin: ["https://godrive-self.vercel.app/", "https://godriveclient.vercel.app/"],
+    origin: ["https://godrive-self.vercel.app", "https://godriveclient.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, 
   })
