2ab0752e8accc351ab08fe225ec836e397ee7d7d
Preeti Dalai <93539836+PD203@users.noreply.github.com>
2025-02-19 12:00:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const rideRoutes = require('./routes/ride')
 connectToDb()
 
 const corsOptions = {
-  origin: "https://godrive-1.onrender.com/", 
+  origin: "*", 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 };
